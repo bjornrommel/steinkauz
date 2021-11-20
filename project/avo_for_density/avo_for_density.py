46fa5d35d8256d62cf926f35ddd7d282a684606e
bjornrommel <88940494+bjornrommel@users.noreply.github.com>
2021-11-20 19:33:54 +0100

    simplified interact
    The interact, interactive, interactive_output calls have been
    simplified. Many overall improvements.
    Continuous work on avo_for_density

--- project/avo_for_density/avo_for_density.py
+++ project/avo_for_density/avo_for_density.py
@@ -1,66 +1,285 @@
 # -*- coding: utf-8 -*-
 """
-Linear AVO-Inversion
+Created on Mon Nov  8 07:52:18 2021
 
-@author: Björn E. Rommel
+@author: Bjorn
 """
 
 
-# libraries
+# import
+import os
+import sys
 import numpy as np
+from numpy.random import default_rng
+import matplotlib.pyplot as plt
+import config as cfg
 
 
-def back_contrast(
-        vp1=None, vp2=None, vs1=None, vs2=None, rho1=None, rho2=None):
-    bg_vp = 0.5 * (vp1 + vp2)
-    bg_vs = 0.5 * (vs1 + vs2)
-    bg_rho = 0.5 * (rho1 + rho2)
-    ct_vp = vp2 - vp1 
-    ct_vs = vs2 - vs1
-    ct_rho = rho2 - rho1 
-    
-def pdown_pup(
-        vp0=None, vs0=None, rho0=None,
-        ct_vp=None, ct_vs=None, ct_rho=None,
-        ang=None):
+# default range of angles for the theoretical AVA curve
+ANG = [float(iii) / 100. for iii in range(0,3001,100)]
+
+
+# parameters for the demo wavelet
+WAVELET = 'triangle'   # type of wavelet
+NWL = 10               # half-width of wavelet
+
+
+# indices
+IVP, IVS, IRHO = 0, 1, 2
+
+
+# debug switch
+DEBUG = True
+
+
+# --- do not change below --- do not change below --- do not change below ---
+
+
+# suppress traceback
+if not DEBUG:
+    sys.tracebacklimit = 0
+
+
+def comp_ava():
     """
-    Calculate linearized Pdown-Pup reflection coefficients.
+    Display a theoretical AVA curve based on given priors.
 
     Parameters
     ----------
-    vp0 : float
-        background P velocity
-    vs0 : float
-        background S velocity
-    rho0 : float
-        background density
-    ct_vp : float
-        P velocity contrast
-    ct_vs : float
-        S velocity contrast
-    ct_rho : float
-        density contrast
-    ang : np.array([float, ...])
-        incidence angles
+    mod : array of floats
+        normalized vp, vs, and density contrasts
+    vsp : float
+        inverse P-to-S velocity
+    angs : array of floats
+        incidence angles, default given
 
     Returns
     -------
-    amp : float
-        linearized Pdown-Pup reflection coefficient
+    None.
 
     """
-    # calculate ray parameter
-    ray = np.sin(ang) / vp0
-    # calculate coefficients
-    coeff = np.full(3, np.NAN)
-    coeff[0] = 0.5 * (1. - 4. * vs0 ** 2 * ray ** 2)
-    coeff[1] = 0.5 / np.cos(ang) ** 2
-    coeff[2] = -4. * vs0 ** 2 * ray ** 2
-    # calculate normalized coefficients
-    coeff[0] /= vp0
-    coeff[1] /= vs0
-    coeff[2] /= rho0
-    # calculate reflection amplitude
-    amp = coeff[0] * ct_vp + coeff[1] * ct_vs + coeff[2] * ct_rho
-    # return
-    return amp
+
+    def catch_data(event=None):
+        # preserve
+        cfg.ang += [event.xdata]
+        print(cfg.amp, type(cfg.amp))
+        cfg.amp += [event.ydata]
+        print('***')
+        print(cfg.ang, cfg.amp)
+        # plot data point
+        plt.plot(event.xdata, event.ydata, 'or')
+        # show curve + data points if any
+        event.canvas.draw()
+        plt.show()
+
+    def onclick(event, vsp=cfg.vsp, snr=cfg.snr, est=cfg.est, prior=cfg.mod):
+        """
+        Mark and preserve clicked points in an AVA plot.
+
+        Parameters
+        ----------
+        event : Mouse_event
+            on-click event
+
+        Returns
+        -------
+        None.
+
+        """
+        # catch event
+        catch_data(event=event)
+        # invert AVA
+        invert_ava()
+
+    # convert angle from degree to radiant
+    rang = np.deg2rad(ANG)
+    # compute normalized Fréchet derivative
+    # (in form of a matrix[angle, component])
+    # (see Aki&Richards, Quantitative Seismology, Theory and Methods, I)
+    gmat = np.array([
+        0.5 / np.cos(rang) ** 2,
+        -4. * cfg.vsp ** 2 * np.sin(rang) ** 2,
+        0.5 * (1. - 4. * cfg.vsp ** 2 * np.sin(rang) ** 2)],
+        dtype=float)
+    gmat = gmat.transpose()
+    # compute reflection coefficients
+    # (in form of a vector[angle])
+    cfg.coeff = np.dot(gmat, cfg.mod)
+    # close old figure
+    plt.close(1)
+    # set figure
+    fig = plt.figure(1)
+    # plot curve
+    plt.plot(ANG, cfg.coeff, 'b')
+    plt.xlim(left=0., right=45.)
+    plt.ylim(bottom=-1., top=+1.)
+    # show figure
+    plt.show()
+    # register clicks on data points
+    fig.canvas.mpl_connect('button_press_event', onclick)
+    # save print figure
+    plt.tight_layout()
+    plt.savefig("curve.eps", dpi=1200, format='eps')
+
+
+def plot_wavelet(wavelet=WAVELET):
+    """
+    Demo a wavelet contaminated with random noise
+
+    Parameters
+    ----------
+    snr : float
+        Signal-to-noise ratio. The default is 0..
+    wavelet : char
+        Type of wavelet. The default is WAVELET defined above.
+
+    Raises
+    ------
+    UserWarning
+        Wavelet must be associated with a script computing it.
+
+    """
+
+    def triangle(nnn=NWL):
+        """
+        Create a triangular wavelet.
+
+        Parameters
+        ----------
+        nnn : int, optional
+            Length of triangle in number of samples. The default is NWL.
+
+        Returns
+        -------
+        signal : list of float
+            Samples of a triangular wavelet.
+
+        """
+        # ramp up
+        signal = [1. / float(nnn) * iii for iii in range(1,nnn+1)]
+        # ramp down
+        signal += [1. / float(nnn) * iii for iii in range(nnn-1,0,-1)]
+        # return
+        return signal
+
+    # define wavelets
+    wavelets = {'triangle': triangle}
+    # close old figure
+    plt.close(1)
+    # set figure
+    plt.figure(1)
+    # create wavelet
+    try:
+        wavelets[wavelet]
+    except KeyError as msg:
+        string = "plot_wavelet: cannot find wavelet {wavelet:}"
+        string = string.format(wavelet=wavelet)
+        print(string)
+        raise UserWarning(msg) from msg
+    # generate normally distributed random noise
+    trace = cfg.snr * default_rng().standard_normal(12*NWL)
+    # mix wavelet and noise
+    trace[2*NWL:4*NWL-1] += wavelets[wavelet](nnn=NWL)
+    # plot data
+    plt.plot(trace)
+    # adjust abscissa
+    plt.xlim(left=1, right=12*NWL)
+    # adjust ordinate
+    plt.ylim(bottom = -0.5, top=1.5)
+    plt.xticks(ticks=[], labels=None)
+    # show figure
+    plt.show()
+    # check directory for wavelet
+    if not os.path.isdir('wavelet'):
+        os.mkdir('wavelet')
+    # save printout
+    plt.tight_layout()
+    plt.savefig('wavelet/wavelet.eps', dpi=1200, format='eps')
+
+
+def invert_ava():
+    print(cfg.amp)
+    if len(cfg.amp) >= 3:
+        # convert to radiant
+        rang = np.deg2rad(cfg.ang)
+        # compute normalized Fréchet derivative
+        # (in form of a matrix[angle, component])
+        # (see Aki&Richards, Quantitative Seismology, Theory and Methods, I)
+        gmat = np.array([
+            0.5 / np.cos(rang) ** 2,
+            -4. * cfg.vsp ** 2 * np.sin(rang) ** 2,
+            0.5 * (1. - 4. * cfg.vsp ** 2 * np.sin(rang) ** 2)],
+            dtype=float)
+        gmat = gmat.transpose()
+        # compute uncertainty
+        # initialize with data-controlled part
+        print(cfg.amp)
+        print(gmat)
+        print(cfg.snr)
+        cfg.post_cov = (
+            np.matmul(
+                np.matmul(
+                    np.transpose(gmat), np.linalg.inv(cfg.snr)),
+                gmat))
+        # add model-controlled part
+        cfg.post_cov += np.linalg.inv(cfg.prior_cov)
+        # invert to posterior uncertainty
+        cfg.post_cov = np.linalg.inv(cfg.post_cov)
+        # initialize data-controlled part of inversion amplitude
+        amp = (
+            np.dot(
+                np.matmul(np.transpose(gmat), np.linalg.inv(cfg.snr)),
+                cfg.amp))
+        # add model-controlled part
+        amp += (
+            np.dot(
+                np.linalg.inv(cfg.prior_cov), cfg.prior_mod))
+        # inversion
+        cfg.post_mod = (
+            np.dot(np.linalg.inv(cfg.post_cov), amp))
+        print(cfg.post_cov)
+        print(cfg.post_mod)
+
+
+if __name__ == '__main__':
+    # Forward AVA simulation requires only the normalized contrasts of 
+    # seismic properties and the S-to-P velocity ratio, not the seismic 
+    # properties themselves. However, it is more intuitive to input seismic
+    # properties. 
+    # -------------------------------------------------------------------------
+    # input seismic properties of the top medium and their uncertainties
+    VP1, DVP1 = 2000, 20
+    VS1, DVS1 = 1500, 20
+    RHO1, DRHO1 = 2400, 50
+    # input properties of the bottom medium
+    VP2, DVP2 = 2000, 20
+    VS2, DVS2 = 1500, 20
+    RHO2, DRHO2 = 2600, 50
+    # input signal-to-noise ratio
+    SNR = 0.05
+    # compute the background
+    cfg.back = np.array([VP1 + VP2, VS1 + VS2, RHO1 + RHO2]) / 2.
+    cfg.vsp = cfg.back[IVS] / cfg.back[IVP]
+    # compute the normalized contrasts -> model, in Bayes' theory prior model,
+    # and the covariance matrix
+    cfg.mod = np.divide(
+        np.array([VP2 - VP1, VS2 - VS1, RHO2 - RHO1]),
+        cfg.back)
+    cfg.prior['mod'] = cfg.mod   # intentionally identical
+    cfg.prior['cov'] = (
+        np.diag(
+            np.array([
+                (VP2 * DVP1) ** 2 + (VP1 * DVP2) ** 2,        # 
+                (VS2 * DVS1) ** 2 + (VS1 * DVS2) ** 2,
+                (RHO2 * DRHO1) ** 2 + (RHO1 * DRHO2) ** 2])
+            /                                                 # element-wise
+            np.power(cfg.back, 4)))                           # back ** 4
+    # compute reflection coefficients as forward problem
+    cfg.coeff['amp'] = []
+    cfg.coeff['ang'] = ANG
+    # record the data and the signal-to-noise ratio -> data covariance
+    cfg.data['amp'] = []
+    cfg.data['ang'] = []
+    cfg.data['cov'] = np.eye(3,3) * SNR ** 2   # covariance
+    # compute AVA curve
+    comp_ava()
---
+++ project/tutorial/example/figure.tpl
@@ -0,0 +1,8 @@
+r"""
+\begin{figure}
+    \begin{minipage}{8cm}
+        \includegraphics[width=\textwidth]{example.eps}
+        \caption{The mean is MEAN.}
+    \end{minipage}
+\end{figure}
+"""
